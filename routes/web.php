2621fb5fcffff2cea6d2fec112e5f3dc9479ef20
Muhamad Ramdani <95694399+Ram2thedani@users.noreply.github.com>
2024-12-07 09:25:27 +0000

    changed default route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/student', [StudentController::class, 'index'])->name('student');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [StudentController::class, 'index'])->name('student');
